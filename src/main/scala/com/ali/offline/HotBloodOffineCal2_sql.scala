e654e30486d7187e8e372be953b8fd63a6a41518
gjh <gjh1589557@163.com>
2019-12-18 21:33:42 +0800

    项目sparksql完善

--- src/main/scala/com/ali/offline/HotBloodOffineCal2_sql.scala
+++ src/main/scala/com/ali/offline/HotBloodOffineCal2_sql.scala
@@ -22,9 +22,9 @@ import scala.collection.mutable.ArrayBuffer
 
 object HotBloodOffineCal2_sql {
 
-  def publicType(sc:SparkContext,filter:RDD[(String, String, String, String)], castdata: Broadcast[String],x:Int,y:Int) = {
-    val (currpent,next)=getCurrentAndNextday(castdata,x,y)
-val broadcast = sc.broadcast((currpent,next))
+  def publicType(sc: SparkContext, filter: RDD[(String, String, String, String)], castdata: Broadcast[String], x: Int, y: Int) = {
+    val (currpent, next) = getCurrentAndNextday(castdata, x, y)
+    val broadcast = sc.broadcast((currpent, next))
     val cal = filter.map(line => {
       //时间
       val time = line._3 //2018年2月1日，星期一，10:02:01
@@ -33,122 +33,126 @@ val broadcast = sc.broadcast((currpent,next))
 
       //事件类型
       val eventType = line._2
-      (timeMills,broadcast.value._1,broadcast.value._2,eventType,line._1)
+      (timeMills, broadcast.value._1, broadcast.value._2, eventType, line._1)
     })
     cal
-//      (timeMills >= currpent && timeMills < next) && (eventType.equals(EventType.REDISTER.getEventType))
-//
-//    }).map(_._1).distinct()
-//    count
+    //      (timeMills >= currpent && timeMills < next) && (eventType.equals(EventType.REDISTER.getEventType))
+    //
+    //    }).map(_._1).distinct()
+    //    count
 
   }
 
-  def calnewuser(sc:SparkContext,filter: RDD[(String, String, String, String)], castdata: Broadcast[String],x:Int,y:Int)={
-    val cal=publicType(sc,filter,castdata,x,y)
-    cal.filter(line=>{
-      (line._1>=line._2&&line._1<line._3)&&(line._4.equals(EventType.REDISTER.getEventType))
+  def calnewuser(sc: SparkContext, filter: RDD[(String, String, String, String)], castdata: Broadcast[String], x: Int, y: Int) = {
+    val cal = publicType(sc, filter, castdata, x, y)
+    cal.filter(line => {
+      (line._1 >= line._2 && line._1 < line._3) && (line._4.equals(EventType.REDISTER.getEventType))
     }).map(_._5).distinct()
   }
 
 
   /**
     * 获得基准日以及任意日的毫秒值用来比较那个注册的基准日和日期
+    *
     * @param filter
     * @param castdata
     * @return
     */
-  def getCurrentAndNextday(castdata: Broadcast[String],x:Int,y:Int) = {
+  def getCurrentAndNextday(castdata: Broadcast[String], x: Int, y: Int) = {
     //这里用了x,y来标榜是基于基准日后的那个时间段，如要基准日到下一天则，casedata,0,1  要求基准日的下一天到下一天
     //则casedata,1,1
     val baseDatestr = castdata.value.concat(" 00:00:00")
-    val sdf=new SimpleDateFormat(CommonUtils.loadApolicationpropertie().getString(CommonData.TIME_PATTERN))
+    val sdf = new SimpleDateFormat(CommonUtils.loadApolicationpropertie().getString(CommonData.TIME_PATTERN))
     val date = sdf.parse(baseDatestr)
     val baseDateMillions = date.getTime
 
 
-    val calendar=Calendar.getInstance()
+    val calendar = Calendar.getInstance()
     calendar.setTime(date)
 
-    calendar.add(Calendar.DATE,x)
+    calendar.add(Calendar.DATE, x)
     //下一天的时间
     val next = calendar.getTime
-    val nextMillions=calendar.getTimeInMillis
+    val nextMillions = calendar.getTimeInMillis
 
-    val calendar1=Calendar.getInstance()
+    val calendar1 = Calendar.getInstance()
 
     calendar1.setTime(next)
-    calendar1.add(Calendar.DATE,y)
+    calendar1.add(Calendar.DATE, y)
 
-    val end=calendar1.getTime
-    val endMillions=calendar1.getTimeInMillis
+    val end = calendar1.getTime
+    val endMillions = calendar1.getTimeInMillis
 
-    (nextMillions,endMillions)
+    (nextMillions, endMillions)
   }
 
-  def calActiveUser(sc:SparkContext,filter: RDD[(String, String, String, String)], castdata: Broadcast[String],x:Int,y:Int) = {
-//    得到当前和下一天的时间
-//    val (currpent,next)=getCurrentAndNextday(castdata,x,y)
-//    val count = filter.filter(line => {
-//      //时间
-//      val time = line._3 //2018年2月1日，星期一，10:02:01
-//      val sdf = new SimpleDateFormat(CommonUtils.loadApolicationpropertie().getString(CommonData.TIME_PATTERN2))
-//      val timeMills = sdf.parse(time).getTime
-//
-//      //事件类型
-//      val eventType = line._2
-//
-//      (timeMills >= currpent && timeMills < next) &&
-//        (eventType.equals(EventType.REDISTER.getEventType)||eventType.equals(EventType.LOGIN.getEventType))
-//
-//    }).map(_._1).distinct()
-//    count
-val cal=publicType(sc,filter,castdata,x,y)
-    cal.filter(line=>{
-      (line._1>=line._2&&line._1<line._3)&&
-        (line._4.equals(EventType.REDISTER.getEventType)||line._4.equals(EventType.LOGIN.getEventType))
+  def calActiveUser(sc: SparkContext, filter: RDD[(String, String, String, String)], castdata: Broadcast[String], x: Int, y: Int) = {
+    //    得到当前和下一天的时间
+    //    val (currpent,next)=getCurrentAndNextday(castdata,x,y)
+    //    val count = filter.filter(line => {
+    //      //时间
+    //      val time = line._3 //2018年2月1日，星期一，10:02:01
+    //      val sdf = new SimpleDateFormat(CommonUtils.loadApolicationpropertie().getString(CommonData.TIME_PATTERN2))
+    //      val timeMills = sdf.parse(time).getTime
+    //
+    //      //事件类型
+    //      val eventType = line._2
+    //
+    //      (timeMills >= currpent && timeMills < next) &&
+    //        (eventType.equals(EventType.REDISTER.getEventType)||eventType.equals(EventType.LOGIN.getEventType))
+    //
+    //    }).map(_._1).distinct()
+    //    count
+    val cal = publicType(sc, filter, castdata, x, y)
+    cal.filter(line => {
+      (line._1 >= line._2 && line._1 < line._3) &&
+        (line._4.equals(EventType.REDISTER.getEventType) || line._4.equals(EventType.LOGIN.getEventType))
     }).map(_._5).distinct()
   }
 
 
-  def calnextActivePretenge(newuser:RDD[String],sc:SparkContext,baseData: Broadcast[String], filter: RDD[(String, String, String, String)],x:Int,y:Int) = {
+  def calnextActivePretenge(newuser: RDD[String], sc: SparkContext, baseData: Broadcast[String], filter: RDD[(String, String, String, String)], x: Int, y: Int) = {
     //未进行优化：19090 不添加广播变量
     //17797广播变量后
 
-    val count1=newuser.count()
+    val count1 = newuser.count()
 
-  val tmp1 = newuser.map(line=>{(line,"")})
-    val activeuser=calActiveUser(sc,filter,baseData,x,y)
-    val count2=activeuser.count()
+    val tmp1 = newuser.map(line => {
+      (line, "")
+    })
+    val activeuser = calActiveUser(sc, filter, baseData, x, y)
+    val count2 = activeuser.count()
 
-      val tmp2 = activeuser.map(line=>(line,""))
+    val tmp2 = activeuser.map(line => (line, ""))
     val join: RDD[(String, (String, String))] = tmp1.join(tmp2)
 
-    val pentenge=join.count().toDouble/count1.toDouble
+    val pentenge = join.count().toDouble / count1.toDouble
     pentenge
 
   }
 
   def save2db(baseData: String, newusertotal: Long, activeUsertotal: Long, array: ArrayBuffer[String]) = {
     //Dao层实例准备
-    val dao:iGameAnalysisResult=new GameAnalysisResultImpl
+    val dao: iGameAnalysisResult = new GameAnalysisResultImpl
     //构建实例
-    val bean=new GameAnalysisResultBean(baseData,newusertotal,activeUsertotal,array(0),array(1),array(2),array(3),array(4))
-      //save
-dao.saveday(bean)
+    val bean = new GameAnalysisResultBean(baseData, newusertotal, activeUsertotal, array(0), array(1), array(2), array(3), array(4))
+    //save
+    dao.saveday(bean)
   }
-def getTimeMills(castdata:String)={
-//  val baseDatestr = castdata.concat(" 00:00:00")
-  val sdf=new SimpleDateFormat(CommonUtils.loadApolicationpropertie().getString(CommonData.TIME_PATTERN2))
-  val date = sdf.parse(castdata)
-  val baseDateMillions = date.getTime
-  baseDateMillions
-}
+
+  def getTimeMills(castdata: String) = {
+    //  val baseDatestr = castdata.concat(" 00:00:00")
+    val sdf = new SimpleDateFormat(CommonUtils.loadApolicationpropertie().getString(CommonData.TIME_PATTERN2))
+    val date = sdf.parse(castdata)
+    val baseDateMillions = date.getTime
+    baseDateMillions
+  }
 
   def main(args: Array[String]): Unit = {
-    val current=System.currentTimeMillis()
+    val current = System.currentTimeMillis()
 
     //1:拦截非法参数
-    if(args==null||args.length!=1){
+    if (args == null || args.length != 1) {
       println(
         """
           |警告：
@@ -156,13 +160,13 @@ def getTimeMills(castdata:String)={
         """.stripMargin)
       sys.exit(-1)
     }
-//    2:2 获得参数
+    //    2:2 获得参数
 
-    val Array(baseData)=args
+    val Array(baseData) = args
 
-//    println(s"基准日：$baseData")
-//sparkSession rdd里面的缓存默认是在内存中的，dataframe的缓存机制是内存和磁盘那个选项
-    val spark= initEnv
+    //    println(s"基准日：$baseData")
+    //sparkSession rdd里面的缓存默认是在内存中的，dataframe的缓存机制是内存和磁盘那个选项
+    val spark = initEnv
 
 
     val sc: SparkContext = spark.sparkContext
@@ -170,7 +174,7 @@ def getTimeMills(castdata:String)={
 
     //从es中读取数据
     import spark.implicits._
-    val filter=ReadEsData(sc).toDF("userName","eventType","time","ip")
+    val filter = ReadEsData(sc).toDF("userName", "eventType", "time", "ip")
     filter.createOrReplaceTempView("tb_tmp")
     spark.sqlContext.cacheTable("tb_tmp")
 
@@ -183,7 +187,7 @@ def getTimeMills(castdata:String)={
     val castdata: Broadcast[String] = sc.broadcast(baseData)
     import org.apache.spark.sql.functions._
     //注册udf自定义函数
-    spark.udf.register("getTime",(time:String)=>getTimeMills(time))
+    spark.udf.register("getTime", (time: String) => getTimeMills(time))
     //注册时间大于今天小于明天
 
     val newUser = spark.sql(
@@ -203,10 +207,10 @@ getTime(time)<UNIX_TIMESTAMP(DATE_ADD('$baseData',1),'yyyy-MM-dd') * 1000)
       * 获取基准日信息
       * 对rdd中消息进行过滤计算，返回
       * 条件：玩游戏时间>基准日<第二天 and 事件类型为注册或登录
- *
- */
-val activeuser = spark.sql(
-  s"""
+      *
+      */
+    val activeuser = spark.sql(
+      s"""
 select
 count(distinct(userName)) `活跃用户`
 from tb_tmp
@@ -225,10 +229,10 @@ getTime(time)<UNIX_TIMESTAMP(DATE_ADD('$baseData',1),'yyyy-MM-dd') * 1000)
       * 基准日后一天的活跃，所以这个可以求出次日留存率，求7日的时候可以将7,1传入此时他就会去计算第七天登陆的活跃用户，然后与基准日
       * 注册用户进行相关联合系列操作
       */
-//    val nextActivePretenge=calnextActivePretenge(adduser,sc,castdata,filter,2,1)
+    //    val nextActivePretenge=calnextActivePretenge(adduser,sc,castdata,filter,2,1)
     //求出七天的留存率
-    val stayFinalResult=ArrayBuffer[String]()
-    for(i<- 1 to 7) {
+    val stayFinalResult = ArrayBuffer[String]()
+    for (i <- 1 to 7) {
       val stayperten = spark.sql(
         s"""
 select
@@ -247,17 +251,16 @@ and (getTime(t2.time)>=UNIX_TIMESTAMP(DATE_ADD('$baseData',$i),'yyyy-MM-dd') * 1
     }
     println(stayFinalResult)
 
-//    save2db(baseData,newusertotal,activeUsertotal,array)
+    //    save2db(baseData,newusertotal,activeUsertotal,array)
 
 
-
-
-    val end=System.currentTimeMillis()
-    println("耗时"+(end-current))
+    val end = System.currentTimeMillis()
+    println("耗时" + (end - current))
 
     spark.stop()
   }
-//初始化spark环境
+
+  //初始化spark环境
   private def initEnv = {
     SparkSession
       .builder()
